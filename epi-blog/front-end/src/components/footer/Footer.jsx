403545f696c4c574109482ca20f24d23fcab0053
Massimiliano <maxtera87@gmail.com>
2025-03-27 20:01:12 +0000

    footer

--- epi-blog/front-end/src/components/footer/Footer.jsx
+++ epi-blog/front-end/src/components/footer/Footer.jsx
@@ -17,8 +17,12 @@ const Footer = () => {
             <h5 className="footer-title">Link Utili</h5>
             <ul className="footer-links">
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/posts/create">Nuovo Post</Link></li>
-              <li><Link to="/myposts">I Miei Post</Link></li>
+              {user && (
+                <>
+                  <li><Link to="/posts/create">Nuovo Post</Link></li>
+                  <li><Link to="/myposts">I Miei Post</Link></li>
+                </>
+              )}
             </ul>
           </Col>
           <Col md={4} className="footer-section">
