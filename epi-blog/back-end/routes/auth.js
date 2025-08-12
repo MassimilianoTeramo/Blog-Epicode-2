e0dda91080ddfa7b4599cd7b2227dc3c84d50a53
Massimiliano <maxtera87@gmail.com>
2025-08-12 19:00:41 +0100

    authorization

--- epi-blog/back-end/routes/auth.js
+++ epi-blog/back-end/routes/auth.js
@@ -7,6 +7,7 @@ import express from 'express';
 import mailer from '../helpers/mailer.js';
 import { authorization } from "../middlewares/authorization.js";
 
+
 const router = Router();
 
 //GENERATE TOKEN
